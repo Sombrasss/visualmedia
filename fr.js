d9592aa43e1e5f0daabe94142866da6d42cb8bde
Sombrasss <86312302+Sombrasss@users.noreply.github.com>
2026-03-02 20:05:21 +0100

    Add files via upload

---
+++ fr.js
@@ -0,0 +1,12 @@
+    const firebaseConfig = {
+  apiKey: "AIzaSyClCpm3891MYMEH0rOQOZNOr2UNRjp_llc",
+  authDomain: "comunidade-2af4a.firebaseapp.com",
+  projectId: "comunidade-2af4a",
+  storageBucket: "comunidade-2af4a.firebasestorage.app",
+  messagingSenderId: "1000455553281",
+  appId: "1:1000455553281:web:14e7bd0c10b5725371db00"
+};
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+
+    
